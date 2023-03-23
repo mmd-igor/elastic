1f59deca5d1e55663a011443fe05d51c0288c6b4
Booster <mmd.igor@gmail.com>
2023-03-23 19:39:16 +0200

    14

--- app/ElasticSearch.php
+++ app/ElasticSearch.php
@@ -45,10 +45,7 @@ class ElasticSearch
         if (is_array($result) && array_key_exists('hits', $result) && $result['hits']['total']['value'] > 0) {
             $result['hits']['hits'][0]['_source']['_meta']['score'] = $result['hits']['hits'][0]['_score'];
             $result['hits']['hits'][0]['_source']['_meta']['method'] = 'M';
-            $result['hits']['hits'][0]['_source']['_meta']['_key']['body'] = json_encode($params['body'], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-            $result['hits']['hits'][0]['_source']['_meta']['_key']['name'] = $name;
-            $result['hits']['hits'][0]['_source']['_meta']['_key']['brand'] = $brand;
-            $result['hits']['hits'][0]['_source']['_meta']['_key']['article'] = $article;
+            $result['hits']['hits'][0]['_source']['_key'] = json_encode($params['body'], JSON_UNESCAPED_UNICODE);
 
             return $result['hits']['hits'][0]['_source'];
         } else {
